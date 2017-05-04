dd5aebadb5aaa52b9637d8801d0852b00192c754
mf <myemail@isawesome.com>
2017-05-04 16:45:29 -0500

    started adding event listeners

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(flash());
 
 
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 require('./helpers/functionsAndFriends');
 
 app.use('/', index);
