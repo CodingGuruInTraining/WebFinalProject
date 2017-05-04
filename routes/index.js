12dbf10af9f30b90f089b49e26de662739fcfed8
mf <myemail@isawesome.com>
2017-05-04 14:20:38 -0500

    created login page and started making routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '_____ Stat Tracker' });
 });
+
+
+
+
+
+// GET login page
+router.get('/login', function(req, res, next) {
+    res.render('login', { title: 'title here' });
+});
+// POST login action
+router.post('/loginAction', passport.authenticate('loginConfig', {
+    goodlogin: '/',
+    badlogin: '/login',
+    failureFlash: true
+}));
+
+
+
+// GET signup page
+router.get('/signup', function(req, res, next) {
+    res.render('signup')
+});
+// POST signup action
+router.post('/signupAction', passport.authenticate('signupConfig', {
+    goodsignup: '/',
+    badsignup: '/signup',
+    failureFlash: true
+}));
+
+
+
+
 
 module.exports = router;
