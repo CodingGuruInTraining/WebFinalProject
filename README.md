390e13020436beb6c1e469a200757bd499f0d9f4
MF <notsure@compuserv.com>
2017-05-12 19:31:19 -0500

    added procfile, updated readme, uncommented ajax code

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# express
+SPEED TYPING TESTER
 
-Just the files created from running `express -e --hbs`.
+This is a simple test to see how quickly and accurately one can type a random string, in this case a random Ron Swanson quote from jamesseanwright's Ron Swanson Quotes API. Each test is recorded with the logged-in user's credentials and shown to all in the high score table!
--- public/javascripts/functionsAndFriends.js
+++ public/javascripts/functionsAndFriends.js
@@ -68,42 +68,42 @@ function addMyListeners() {
  * TABLE FILL METHODS
  *
  * */
-// function fillTable() {
-//     $.ajax({
-//         method:"GET",
-//         url:"fillAll"
-//     }).done(function(data) {
-//         addEachRow(data);
-//     }).fail(function(error) {
-//         console.log("error occurred:");
-//         console.log(error);
-//     })
-// }
-//
-// function addEachRow(data) {
-//     var table = $('#dataTable');
-//     for (var i = 0; i < data.length; i++) {
-//         addRow(data[i], table);
-//     }
-// }
-//
-// function addRow(data, table) {
-// // TODO figure out what is being passed and fill in row
-// // TODO may need to modify what is sent over somehow
-// console.log('addRow about to crash');
-//     var row = '<tr>' +
-//         '<td>' + data.username + '</td>' +
-//         '<td>' + data.time + '</td>' +
-//         '<td>' + data.something1 + '</td>' +
-//         '<td>' + data.something2 + '</td>' +
-//         '<td>' + data.something3 + '</td>' +
-//         '</tr>';
-//     table.append(row);
-// }
+function fillTable() {
+    $.ajax({
+        method:"GET",
+        url:"fillAll"
+    }).done(function(data) {
+        addEachRow(data);
+    }).fail(function(error) {
+        console.log("error occurred:");
+        console.log(error);
+    })
+}
+
+function addEachRow(data) {
+    var table = $('#dataTable');
+    for (var i = 0; i < data.length; i++) {
+        addRow(data[i], table);
+    }
+}
+
+function addRow(data, table) {
+// TODO figure out what is being passed and fill in row
+// TODO may need to modify what is sent over somehow
+console.log('addRow about to crash');
+    var row = '<tr>' +
+        '<td>' + data.username + '</td>' +
+        '<td>' + data.time + '</td>' +
+        '<td>' + data.something1 + '</td>' +
+        '<td>' + data.something2 + '</td>' +
+        '<td>' + data.something3 + '</td>' +
+        '</tr>';
+    table.append(row);
+}
 /*****************/
 
 addMyListeners();
-// fillTable();
+fillTable();
 
 // helpers:
 // http://stackoverflow.com/questions/109086/stop-setinterval-call-in-javascript
